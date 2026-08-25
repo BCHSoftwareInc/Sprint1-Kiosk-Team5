8b6467963dc5447f4ba1f26d2fd2348c71db79dc
eatingmaster123 <bdfow905@learner.dvusd.org>
2026-08-25 09:25:28 -0700

    made program
    started the program 🤑🤑🥶

---
+++ python/ksoiK/ksoiK/ksoiK.py
@@ -0,0 +1,29 @@
+print ("What is your name?")
+name = input()
+print (name)
+
+print ("What is your organization?")
+organization = input()
+print (organization)
+
+print ("What is your Email?")
+Email = input()
+print (Email)
+
+print ("What is your access level?")
+access = input()
+print (access)
+
+print ("Who is your issuer?")
+issuer = input()
+print (issuer)
+
+print ("+--------------------------------+")
+print ("|     APEX ENTERTAINMENT PASS    |")
+print ("|--------------------------------|")
+print ("|   Attendee : " + name.ljust(18) + "|")
+print ("|   Org : " + organization + "   |")
+print ("|   Contact : " + Email + "      |")
+print ("|   Access : " + access + "      |")
+print ("|   Issuer : " + issuer + "      |")
+print ("+--------------------------------+")
